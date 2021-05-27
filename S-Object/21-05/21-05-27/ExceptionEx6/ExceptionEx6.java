8a958735b37e12aad89a79530c43bc78bd5a7b26
KeunYoung-Yang <kiju0242@gmail.com>
2021-05-27 13:10:33 +0900

    21-05-27

---
+++ S-Object/21-05/21-05-27/ExceptionEx6/ExceptionEx6.java
@@ -0,0 +1,17 @@
+
+public class ExceptionEx6 {
+	public static void main(String args[]) {
+		System.out.println(1);
+		System.out.println(2);
+		try {
+			System.out.println(3);
+			System.out.println(0/0);	//0으로 나눠서 ArithmeticException 을 발생시킨다.
+			System.out.println(4); 	// 실행되지 않는다.
+		} catch (Exception e)	{ 	// ArithmeticExcpeiton대신 Exception을 사용.
+			System.out.println(5);
+		}	//try-catch 의 끝
+			System.out.println(6);
+		}	// 	main 메서드의 끝
+	}
+
+
