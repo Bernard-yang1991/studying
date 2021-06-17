fd02a76c0c7dec02db40137c6ec16ace795775c1
KeunYoung-Yang <kiju0242@gmail.com>
2021-06-17 14:35:07 +0900

    21.06.17

--- S-Object/21-06/21.06.17/WhiyMethod.java
+++ S-Object/21-06/21.06.17/WhiyMethod.java
@@ -2,18 +2,21 @@
 public class WhiyMethod {
 
 		public static void main(String[] args)	{
-			
+					
+								//인자, argument
 			//100000000
-			printTwoTimesA();
+			printTwoTimesA("a", "-");
 			//100000000
-			printTwoTimesA();
+			printTwoTimesA("a", "*");
 			//100000000
-			printTwoTimesA();
+			printTwoTimesA("a",	"&");
+			printTwoTimesA("b", "!");
 		}
 		
-		public static void printTwoTimesA() 	{
-			System.out.println("-");
-			System.out.println("a");
-			System.out.println("a");
+											//매개변수, parameter
+		public static void printTwoTimesA(String text, String delimiter) 	{
+			System.out.println(delimiter);
+			System.out.println(text);
+			System.out.println(text);
 		}
 }
