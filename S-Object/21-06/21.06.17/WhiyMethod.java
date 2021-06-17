7f820bf835479ef253b5fb1d94b605676b9aa0b6
KeunYoung-Yang <kiju0242@gmail.com>
2021-06-17 11:19:24 +0900

    21.06.17

---
+++ S-Object/21-06/21.06.17/WhiyMethod.java
@@ -0,0 +1,19 @@
+
+public class WhiyMethod {
+
+		public static void main(String[] args)	{
+			
+			//100000000
+			printTwoTimesA();
+			//100000000
+			printTwoTimesA();
+			//100000000
+			printTwoTimesA();
+		}
+		
+		public static void printTwoTimesA() 	{
+			System.out.println("-");
+			System.out.println("a");
+			System.out.println("a");
+		}
+}
